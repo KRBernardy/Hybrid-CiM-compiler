aee2d28e561fe3cae4cfd28dfa52b4110aa2a301
KRBernardy <krbernardy@outlook.com>
2025-10-25 19:16:11 +0800

    update quant parameters

--- include/linear.h
+++ include/linear.h
@@ -11,9 +11,9 @@
 
 #include "puma.h"
 
-static Vector linear(Model model, std::string layerName, unsigned int in_size, unsigned int out_size, Vector in, bool has_bias = true, unsigned int storageType = 1) {
+static Vector linear(Model model, std::string layerName, unsigned int in_size, unsigned int out_size, Vector in, bool has_bias = true, unsigned int storageType = 1, float activation_scale=1.0f, float weights_scale=1.0f, int activation_zero_point=0, int weights_zero_point=0) {
 
-    ConstantMatrix weight = ConstantMatrix::create(model, layerName + "_weight", in_size, out_size, storageType);
+    ConstantMatrix weight = ConstantMatrix::create(model, layerName + "_weight", in_size, out_size, storageType, activation_scale, weights_scale, activation_zero_point, weights_zero_point);
     if (has_bias) {
         ConstantVector bias = ConstantVector::create(model, layerName + "_bias", out_size);
         return weight * in + bias;
